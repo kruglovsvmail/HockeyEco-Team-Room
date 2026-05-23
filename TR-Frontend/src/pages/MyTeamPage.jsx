e159fa8f7061aff676efcb097522666a42a68aca
kruglovsvmail <kruglov.svmail@gmail.com>
2026-05-23 14:01:28 +0500

    2305 1400

--- TR-Frontend/src/pages/MyTeamPage.jsx
+++ TR-Frontend/src/pages/MyTeamPage.jsx
@@ -331,7 +331,7 @@ export const MyTeamPage = () => {
         </style>
 
         {/* Шапка профиля команды */}
-        <div className="bg-surface-base px-5 pb-4 mb-2 rounded-b-3xl flex items-center gap-4 shadow-lg shrink-0 border-b border-surface-level2 snap-start">
+        <div className="bg-surface-base pt-3 px-5 pb-4 mb-2 rounded-b-3xl flex items-center gap-4 shadow-lg shrink-0 border-b border-surface-level2 snap-start">
           <div className="w-16 h-16 rounded-2xl flex items-center justify-center overflow-hidden drop-shadow-sm shrink-0 ml-4">
             <img src={getImageUrl(selectedTeam?.logo_url)} alt={selectedTeam?.name} className="w-full h-full object-contain p-1" />
           </div>
